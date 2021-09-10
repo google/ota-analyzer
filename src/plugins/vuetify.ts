8a12a6e204e5d99960aa0f093783db0530fed448
Kelvin Zhang <zhangxp1998@gmail.com>
2021-09-09 20:29:08 -0700

    Remove mdi fonts

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/lib/styles/main.sass'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/lib/components'
@@ -22,5 +21,5 @@ import * as directives from 'vuetify/lib/directives'
 
 export default createVuetify({
   components,
-  directives,
+  directives
 })
